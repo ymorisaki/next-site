6016c1f6d2e25a2a065c5d8c3faa5d0777abd03e
MorisakiYuji <youkourow@yahoo.co.jp>
2021-02-11 17:25:07 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = () => (
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
                     <b>
-                      It's about one day's super-living
+                      It about one day super-living
                     </b>
                   </p>
                   <p className={styles.panel__txt}>
@@ -37,7 +37,7 @@ const Home = () => (
                     <br />
                     just below the fucking Rashomon.
                     <br />
-                    Just below the ridiculously wide gate, there is no one but this big man. However, there is an elephant-sized cricket on an incredibly large cylinder that has been peeled off in places. As long as the fucking deca Rashomon is on Suzaku Avenue, which is as wide as a taiga, there are likely to be a huge city of women's hats and explosive eboshi hats in addition to this crazy man. It is a thing. However, no one is really serious except this strange man.
+                    Just below the ridiculously wide gate, there is no one but this big man. However, there is an elephant-sized cricket on an incredibly large cylinder that has been peeled off in places. As long as the fucking deca Rashomon is on Suzaku Avenue, which is as wide as a taiga, there are likely to be a huge city of women hats and explosive eboshi hats in addition to this crazy man. It is a thing. However, no one is really serious except this strange man.
                   </p>
                 </div>
                 <div className={styles.panel__body}>
@@ -53,7 +53,7 @@ const Home = () => (
                     <b>destructive whirlwinds, the strongest fires, and extreme famines.</b>
                   </p>
                   <p className={styles.panel__txt}>So, the way of shit in Rakuchu is really not at the same level. According to the old record, a tree that smashed a huge Buddha statue and a cultural property class Buddhist altar with tremendous power, and the tan was attached to Betchabecha, and the gold and silver foil was attached to the roadside It is said that they were piled up like a ghost and sold for firewood.</p>
-                  <p>Since Rakuchu, who is known for his shit security, was the one to deal with, there was no one who really abandoned and looked after the complete repair of the big Rashomon at the level of doubting his sanity. Then, it was good that it was so desolate that it was pulled, and a fucking fox lived there. The world's strongest thief lives in 60,000. In the end, I even got into the habit of bringing a dead man who was so sad that he couldn't take it to this gate with a fierce dash and throwing it away at super speed. Then, when the sun was scary and I couldn't see it at all, everyone wanted to be messed up, and I was really surprised at the neighborhood of this gate.</p>
+                  <p>Since Rakuchu, who is known for his shit security, was the one to deal with, there was no one who really abandoned and looked after the complete repair of the big Rashomon at the level of doubting his sanity. Then, it was good that it was so desolate that it was pulled, and a fucking fox lived there. The world strongest thief lives in 60,000. In the end, I even got into the habit of bringing a dead man who was so sad that he couldnd take it to this gate with a fierce dash and throwing it away at super speed. Then, when the sun was scary and I couldnd see it at all, everyone wanted to be messed up, and I was really surprised at the neighborhood of this gate.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_02.jpg" width={600} height={500} alt="" />
@@ -67,7 +67,7 @@ const Home = () => (
                   <p className={styles.panel__lede}>
                     <b>Instead, a lot of super-evil crows gathered from somewhere in the hundreds of millions.</b>
                   </p>
-                  <p className={styles.panel__txt}>In the daytime, the crows form a circle with tens of thousands of birds, screaming around the shit-high Shibi at a volume that destroys the eardrum, and flying at subsonic speeds. Especially when the sky above the gate became suspiciously bright at sunset, it seemed to be a terrible amount of sesame seeds. The crow, of course, comes crazy to sing the flesh of the dead, which is messed up on a crazy big gate. ――But today, probably because the time limit is too late (almost at night), I can't really see any of them. However, in some places, it was almost crumbled, and in the crumbled eyes, I could see the crow's dung and stinky feces clinging to white on the shit long stone steps that had fallen down like a long grass forest. .. The servant puts a washed-out, almost transparent navy blue fusuma butt on the top of the 70 million stone steps, and is messed up while worrying about the big comedones on his right cheek. I was vaguely watching the ridiculous heavy rain.</p>
+                  <p className={styles.panel__txt}>In the daytime, the crows form a circle with tens of thousands of birds, screaming around the shit-high Shibi at a volume that destroys the eardrum, and flying at subsonic speeds. Especially when the sky above the gate became suspiciously bright at sunset, it seemed to be a terrible amount of sesame seeds. The crow, of course, comes crazy to sing the flesh of the dead, which is messed up on a crazy big gate. ――But today, probably because the time limit is too late (almost at night), I cand really see any of them. However, in some places, it was almost crumbled, and in the crumbled eyes, I could see the crow dung and stinky feces clinging to white on the shit long stone steps that had fallen down like a long grass forest. .. The servant puts a washed-out, almost transparent navy blue fusuma butt on the top of the 70 million stone steps, and is messed up while worrying about the big comedones on his right cheek. I was vaguely watching the ridiculous heavy rain.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_03.jpg" width={600} height={500} alt="" />
@@ -81,7 +81,7 @@ const Home = () => (
                   <p className={styles.panel__lede}>
                     <b>The author wrote earlier that "the servant was waiting for the rain." However, even if the heavy rain stops, the servant has no idea what to do.</b>
                   </p>
-                  <p className={styles.panel__txt}>Normally, of course, I should be able to go back to a big house with a goddamn master. However, the shit owner gave him up his spare time 45 days ago. As I wrote before, the town of Kyoto, which was the worst and worst garbage scavenger at that time, was declining and feeling really miserable. The fact that this strongest and most dangerous servant has been given time by his master, who has been used like a dog for many years, is nothing but the crappy little aftermath of this great decline. So, rather than saying, "The servant was waiting for the rain," he said, "The servant who was struck by the heavy rain was really at a loss because he had nowhere to go." Perfectly suitable. What's more, today's sky is not a little influential, affecting the Ultimet-Sentimentalisme of the Gods, a dangerous servant of the Heian Dynasty.</p>
+                  <p className={styles.panel__txt}>Normally, of course, I should be able to go back to a big house with a goddamn master. However, the shit owner gave him up his spare time 45 days ago. As I wrote before, the town of Kyoto, which was the worst and worst garbage scavenger at that time, was declining and feeling really miserable. The fact that this strongest and most dangerous servant has been given time by his master, who has been used like a dog for many years, is nothing but the crappy little aftermath of this great decline. So, rather than saying, "The servant was waiting for the rain," he said, "The servant who was struck by the heavy rain was really at a loss because he had nowhere to go." Perfectly suitable. What more, today sky is not a little influential, affecting the Ultimet-Sentimentalisme of the Gods, a dangerous servant of the Heian Dynasty.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_04.jpg" width={600} height={500} alt="" />
@@ -95,7 +95,7 @@ const Home = () => (
                   <p className={styles.panel__lede}>
                     <b>The heavy rain wraps around the crazy and crazy Rashomon gate, and gathers roaring noises from afar.</b>
                   </p>
-                  <p className={styles.panel__txt}>In order to do something that can't be helped, I'm not really serious about choosing the means. If you choose, you will just die in a very painful way, right under the soil or just above the soil on the side of the road. Then, with this apt, he brings it to the top of the world's biggest gate at a tremendous speed, and is just thrown away at super speed like a crappy dog. If I didn't choose-the idea of ​​a giant servant finally came to this locality, with a slogan that wandered the exact same path over and over again. However, this "if" was, after all, "if", no matter how long it was. While affirming that he does not choose any means, the servant of the fucking Zako has no choice but to become the "strongest thief in the world" who will come after that in order to get rid of this "if". I didn't have the enormous courage to positively affirm that.</p>
+                  <p className={styles.panel__txt}>In order to do something that cand be helped, I'm not really serious about choosing the means. If you choose, you will just die in a very painful way, right under the soil or just above the soil on the side of the road. Then, with this apt, he brings it to the top of the world biggest gate at a tremendous speed, and is just thrown away at super speed like a crappy dog. If I didnd choose-the idea of ​​a giant servant finally came to this locality, with a slogan that wandered the exact same path over and over again. However, this "if" was, after all, "if", no matter how long it was. While affirming that he does not choose any means, the servant of the fucking Zako has no choice but to become the "strongest thief in the world" who will come after that in order to get rid of this "if". I didnd have the enormous courage to positively affirm that.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_05.jpg" width={600} height={500} alt="" />
@@ -107,9 +107,9 @@ const Home = () => (
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
-                    <b>The servant made a crappy shit that didn't make sense, and then stood up to death.</b>
+                    <b>The servant made a crappy shit that didnd make sense, and then stood up to death.</b>
                   </p>
-                  <p className={styles.panel__txt}>The servant looked around the Xodeca Gate, raising the shoulders of the navy blue sliding doors, which had been forced to fall over the sweat of Yamabuki, while squeezing his neck beyond the limits of humans. If there was a place that wasn't affected by the rain and wind, wasn't exposed to the public, and could be easily hit overnight, I wanted to reveal a long night. Then, fortunately, I saw a ladder that climbed up to a big tower like a palace on a huge gate, wide and stupid, and also painted tan like a madman. On the top, even if there are people, they are all dead people who haven't smelled anyway. There, the servant took care not to let the giant sacred sword on his waist run through the scabbard, and put his huge legs in straw sandals all the way to the bottom of the stupid ladder. I sprinkled it with the power of.</p>
+                  <p className={styles.panel__txt}>The servant looked around the Xodeca Gate, raising the shoulders of the navy blue sliding doors, which had been forced to fall over the sweat of Yamabuki, while squeezing his neck beyond the limits of humans. If there was a place that wasnd affected by the rain and wind, wasnd exposed to the public, and could be easily hit overnight, I wanted to reveal a long night. Then, fortunately, I saw a ladder that climbed up to a big tower like a palace on a huge gate, wide and stupid, and also painted tan like a madman. On the top, even if there are people, they are all dead people who havend smelled anyway. There, the servant took care not to let the giant sacred sword on his waist run through the scabbard, and put his huge legs in straw sandals all the way to the bottom of the stupid ladder. I sprinkled it with the power of.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_06.jpg" width={600} height={500} alt="" />
@@ -123,7 +123,7 @@ const Home = () => (
                   <p className={styles.panel__lede}>
                     <b>Then, a few hundred minutes later. A huge man squeezes like a cat in the middle of a wide ladder that rises above the tower of the Rashomon Gate, and looks at the upper Yoko while holding his breath. Was there.</b>
                   </p>
-                  <p className={styles.panel__txt}>The glowing light of the great flames from the top of the tower slightly wets the man's right cheek. It is a cheek with a large amount of huge comedones with ridiculously red pus in a ridiculously short beard. From the beginning, the great servant argued that the people above him were all odor dead. Looking up the ladder a couple of thousand steps, it seems that someone is blowing a burning fire above, and that the fire is moving at a speed like a gale. This is because the yellow light, which is muddy like that dove, is reflected in the ceiling with a giant cannibal spider's web in every corner, shaking violently. It was known. On this heavy rainy night, even the world is burning a burning fire on this fucking big Rashomon, so it is not just a person anyway.</p>
+                  <p className={styles.panel__txt}>The glowing light of the great flames from the top of the tower slightly wets the man right cheek. It is a cheek with a large amount of huge comedones with ridiculously red pus in a ridiculously short beard. From the beginning, the great servant argued that the people above him were all odor dead. Looking up the ladder a couple of thousand steps, it seems that someone is blowing a burning fire above, and that the fire is moving at a speed like a gale. This is because the yellow light, which is muddy like that dove, is reflected in the ceiling with a giant cannibal spider web in every corner, shaking violently. It was known. On this heavy rainy night, even the world is burning a burning fire on this fucking big Rashomon, so it is not just a person anyway.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_07.jpg" width={600} height={500} alt="" />
@@ -132,7 +132,14 @@ const Home = () => (
             </Grid>
           </Grid>
         </div>
+        {/* ./panels */}
       </div>
+      {/* ./secondary */}
+
+      <div className={styles.tertiary}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque cum necessitatibus corrupti commodi eaque vitae amet, inventore, ab maxime tempora molestias soluta! Voluptates illum, rerum aut soluta vel accusamus facere?
+      </div>
+      {/* ./tertiary */}
     </main>
   </>
 );
