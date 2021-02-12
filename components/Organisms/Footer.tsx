d5f8c0cf41a305b1f9b00631a34d4e07a7f81cea
MorisakiYuji <youkourow@yahoo.co.jp>
2021-02-12 14:05:00 +0900

    TOPページ実装

---
+++ components/Organisms/Footer.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './Footer.module.scss';
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <ul className={styles.footer__links}>
+      <li>
+        <Link href="/">About</Link>
+        <ul className={`${styles.footer__links} ${styles.footer__childLinks}`}>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+          <li>
+            <Link href="/">PiyoPiyo</Link>
+          </li>
+          <li>
+            <Link href="/">FugaFugaFugaFuga</Link>
+          </li>
+          <li>
+            <Link href="/">FugaFuga</Link>
+          </li>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+          <li>
+            <Link href="/">PiyoPiyoPiyo</Link>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <Link href="/">Pages</Link>
+        <ul className={`${styles.footer__links} ${styles.footer__childLinks}`}>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+          <li>
+            <Link href="/">PiyoPiyo</Link>
+          </li>
+          <li>
+            <Link href="/">Fuga</Link>
+          </li>
+          <li>
+            <Link href="/">FugaFuga</Link>
+          </li>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+          <li>
+            <Link href="/">Piyo</Link>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <Link href="/">Products</Link>
+        <ul className={`${styles.footer__links} ${styles.footer__childLinks}`}>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+          <li>
+            <Link href="/">PiyoPiyo</Link>
+          </li>
+          <li>
+            <Link href="/">FugaFugaFugaFuga</Link>
+          </li>
+          <li>
+            <Link href="/">PiyoPiyoPiyo</Link>
+          </li>
+          <li>
+            <Link href="/">FugaFuga</Link>
+          </li>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <Link href="/">Support</Link>
+        <ul className={`${styles.footer__links} ${styles.footer__childLinks}`}>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+          <li>
+            <Link href="/">PiyoPiyo</Link>
+          </li>
+          <li>
+            <Link href="/">FugaFugaFuga</Link>
+          </li>
+          <li>
+            <Link href="/">FugaFuga</Link>
+          </li>
+          <li>
+            <Link href="/">Piyo</Link>
+          </li>
+          <li>
+            <Link href="/">Hoge</Link>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+    <p className={styles.copy}>
+      <small>
+        <a href="https://github.com/ymorisaki" target="_blank" rel="noreferrer">ymorisaki</a>
+        {' '}
+        ALL rights reserved.
+      </small>
+    </p>
+  </footer>
+);
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 import Grid from '@material-ui/core/Grid';
 import Header from '../components/Organisms/Header';
+import Footer from '../components/Organisms/Footer';
 import HeaderMovie from '../components/Molecules/HeaderMovie';
 import Media from '../components/Atoms/Media';
 import styles from './index.module.scss';
@@ -13,7 +16,7 @@ const Home = () => (
     </Head>
     <Header />
 
-    <main className={styles.hoge}>
+    <main>
       <HeaderMovie />
 
       <div className={styles.secondary}>
@@ -21,10 +24,11 @@ const Home = () => (
           <span className={styles.secondary__hdgSub}>The first candy my grandfather gave me,</span>
           it was Werthers Original and I was 4 years old
         </h2>
+        {/* ./secondary__hdg */}
 
         <div className={styles.panels}>
           <Grid container spacing={3}>
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={12} sm={4}>
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
@@ -45,8 +49,9 @@ const Home = () => (
                 </div>
               </div>
             </Grid>
+            {/* Grind itme */}
 
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={12} sm={4}>
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
@@ -60,8 +65,9 @@ const Home = () => (
                 </div>
               </div>
             </Grid>
+            {/* Grind itme */}
 
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={12} sm={4}>
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
@@ -74,36 +80,39 @@ const Home = () => (
                 </div>
               </div>
             </Grid>
+            {/* Grind itme */}
 
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={12} sm={4}>
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
-                    <b>The author wrote earlier that "the servant was waiting for the rain." However, even if the heavy rain stops, the servant has no idea what to do.</b>
+                    <b>The author wrote earlier that &rdquo;the servant was waiting for the rain.&rdquo; However, even if the heavy rain stops, the servant has no idea what to do.</b>
                   </p>
-                  <p className={styles.panel__txt}>Normally, of course, I should be able to go back to a big house with a goddamn master. However, the shit owner gave him up his spare time 45 days ago. As I wrote before, the town of Kyoto, which was the worst and worst garbage scavenger at that time, was declining and feeling really miserable. The fact that this strongest and most dangerous servant has been given time by his master, who has been used like a dog for many years, is nothing but the crappy little aftermath of this great decline. So, rather than saying, "The servant was waiting for the rain," he said, "The servant who was struck by the heavy rain was really at a loss because he had nowhere to go." Perfectly suitable. What more, today sky is not a little influential, affecting the Ultimet-Sentimentalisme of the Gods, a dangerous servant of the Heian Dynasty.</p>
+                  <p className={styles.panel__txt}>Normally, of course, I should be able to go back to a big house with a goddamn master. However, the shit owner gave him up his spare time 45 days ago. As I wrote before, the town of Kyoto, which was the worst and worst garbage scavenger at that time, was declining and feeling really miserable. The fact that this strongest and most dangerous servant has been given time by his master, who has been used like a dog for many years, is nothing but the crappy little aftermath of this great decline. So, rather than saying, &rdquo;The servant was waiting for the rain,&rdquo; he said, &rdquo;The servant who was struck by the heavy rain was really at a loss because he had nowhere to go.&rdquo; Perfectly suitable. What more, today sky is not a little influential, affecting the Ultimet-Sentimentalisme of the Gods, a dangerous servant of the Heian Dynasty.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_04.jpg" width={600} height={500} alt="" />
                 </div>
               </div>
             </Grid>
+            {/* Grind itme */}
 
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={12} sm={4}>
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
                     <b>The heavy rain wraps around the crazy and crazy Rashomon gate, and gathers roaring noises from afar.</b>
                   </p>
-                  <p className={styles.panel__txt}>In order to do something that cand be helped, I'm not really serious about choosing the means. If you choose, you will just die in a very painful way, right under the soil or just above the soil on the side of the road. Then, with this apt, he brings it to the top of the world biggest gate at a tremendous speed, and is just thrown away at super speed like a crappy dog. If I didnd choose-the idea of ​​a giant servant finally came to this locality, with a slogan that wandered the exact same path over and over again. However, this "if" was, after all, "if", no matter how long it was. While affirming that he does not choose any means, the servant of the fucking Zako has no choice but to become the "strongest thief in the world" who will come after that in order to get rid of this "if". I didnd have the enormous courage to positively affirm that.</p>
+                  <p className={styles.panel__txt}>In order to do something that cand be helped, I&rsquo;m not really serious about choosing the means. If you choose, you will just die in a very painful way, right under the soil or just above the soil on the side of the road. Then, with this apt, he brings it to the top of the world biggest gate at a tremendous speed, and is just thrown away at super speed like a crappy dog. If I didnd choose-the idea of giant servant finally came to this locality, with a slogan that wandered the exact same path over and over again. However, this &rdquo;if&rdquo; was, after all, &rdquo;if&rdquo;, no matter how long it was. While affirming that he does not choose any means, the servant of the fucking Zako has no choice but to become the &rdquo;strongest thief in the world&rdquo; who will come after that in order to get rid of this &rdquo;if&rdquo;. I didnd have the enormous courage to positively affirm that.</p>
                 </div>
                 <div className={styles.panel__body}>
                   <Media src="/images/index_img_05.jpg" width={600} height={500} alt="" />
                 </div>
               </div>
             </Grid>
+            {/* Grind itme */}
 
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={12} sm={4}>
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
@@ -116,8 +125,9 @@ const Home = () => (
                 </div>
               </div>
             </Grid>
+            {/* Grind itme */}
 
-            <Grid item xs={6} sm={4}>
+            <Grid item xs={12} sm={4}>
               <div className={styles.panel}>
                 <div className={styles.panel__head}>
                   <p className={styles.panel__lede}>
@@ -130,17 +140,127 @@ const Home = () => (
                 </div>
               </div>
             </Grid>
+            {/* Grind itme */}
           </Grid>
+          {/* Grind root */}
         </div>
         {/* ./panels */}
       </div>
       {/* ./secondary */}
 
       <div className={styles.tertiary}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque cum necessitatibus corrupti commodi eaque vitae amet, inventore, ab maxime tempora molestias soluta! Voluptates illum, rerum aut soluta vel accusamus facere?
+        <h2 className={styles.tertiary__hdg}>
+          <span className={styles.tertiary__hdgSub}>At first glance, the old woman jumped high in the sky, </span>
+          as if she had been hit by a crossbow.
+        </h2>
+        {/* ./tertiary__hdg */}
+
+        <p className={styles.tertiary__txt}>
+          As the hair fell out one by one, the fear gradually disappeared from the rotten heart of the servant. Then, at the same time,
+          <br />
+          the unimaginable hatred of this old woman gradually moved. ――No, there may be too much misunderstanding when it comes to this old woman.
+          <br />
+          Rather, a huge antipathy to all possible evils in the world has increased geometrically every minute.
+          <br />
+          At this time, someone was thinking of a servant who embodies this strongest justice, just under the gate, and this rotten dove man had a super-painful death or became the world&rsquo;s strongest thief king.
+        </p>
+        <p className={styles.tertiary__txt}>
+          If I bring up the big problem again, it is likely that the innocent, innocent, super-noble servant chose a really painful death without any seriously unskilled pieces.
+          <br />
+          So much, the heart that hates all the evils of this man in the world was burning up like a large piece of the finest pine tree that was inserted into the floor of an old woman.
+        </p>
+
+        <table className={styles.tertiary__table}>
+          <colgroup>
+            <col className="w-20" />
+            <col />
+            <col />
+            <col />
+          </colgroup>
+          <thead>
+            <tr>
+              <td>&nbsp;</td>
+              <th>Free</th>
+              <th>10$</th>
+              <th>50$</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Cloud</th>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+            </tr>
+            <tr>
+              <th>Deploy</th>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+            </tr>
+            <tr>
+              <th>Secure</th>
+              <td>-</td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+            </tr>
+            <tr>
+              <th>Repository</th>
+              <td>-</td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+            </tr>
+            <tr>
+              <th>Backup</th>
+              <td>-</td>
+              <td>-</td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+            </tr>
+            <tr>
+              <th>Database</th>
+              <td>-</td>
+              <td>-</td>
+              <td><Image src="/images/check.svg" width={24} height={24} alt="supported" /></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
       {/* ./tertiary */}
+
+      <div className={styles.conversion}>
+        <h2 className={styles.conversion__hdg}>
+          Let&rsquo;s Try Now!!
+        </h2>
+
+        <ul className={styles.conversion__btns}>
+          <li className={styles.conversion__list}>
+            <Link href="/">Store</Link>
+          </li>
+          <li className={`${styles.conversion__list} ${styles.conversion__listContact}`}>
+            <Link href="/">Contact us</Link>
+          </li>
+        </ul>
+        {/* ./conversion__btns */}
+        <p className={styles.conversion__txt}>
+          The glowing light of the great flames from the top of the tower slightly wets the man right cheek.
+          <br />
+          It is a cheek with a large amount of huge comedones with ridiculously red pus in a ridiculously short beard.
+          <br />
+          From the beginning, the great servant argued that the people above him were all odor dead.
+          <br />
+          Looking up the ladder a couple of thousand steps, it seems that someone is blowing a burning fire above, and that the fire is moving at a speed like a gale.
+          <br />
+          This is because the yellow light, which is muddy like that dove, is reflected in the ceiling with a giant cannibal spider web in every corner, shaking violently.
+          <br />
+          It was known.
+          <br />
+          On this heavy rainy night, even the world is burning a burning fire on this fucking big Rashomon, so it is not just a person anyway.
+        </p>
+      </div>
+      {/* ./conversion */}
     </main>
+
+    <Footer />
   </>
 );
 
